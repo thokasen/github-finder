c2dcf1fbcb179f5e5140dde2f5bfd3613f315d92
unknown <vaag1991@gmail.com>
2019-11-28 19:44:08 +0100

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const UserItem = ({ user: { login, html_url, avatar_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
+
+// class UserItem extends Component {
+//   render() {
+//     const { login, html_url, avatar_url } = this.props.user;
+//     return (
+//       <div className='card text-center'>
+//         <img
+//           src={avatar_url}
+//           alt=''
+//           className='round-img'
+//           style={{ width: "60px" }}
+//         />
+//         <h3>{login}</h3>
+//         <div>
+//           <a href={html_url} className='btn btn-dark btn-sm my-1'>
+//             More
+//           </a>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default UserItem;
